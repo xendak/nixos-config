654c1e987a8a2139aea2c5afc927bc7fbd61dfaa
Rafael Grossi <rg.grossi@outlook.com>
2024-04-30 15:42:27 -0300

    trying bitwarden and agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   flakes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgDWOC2pl8MY/RZJDTsJv1V6H4SjXt6x+UCu4hQFSbs";
-  # drops = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJ3y03wBlzSp0tooMOILl7hGxIYqxT6IjOZWLzDpZHP rg.grossi@outlook.com";
-  drops = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJ3y03wBlzSp0tooMOILl7hGxIYqxT6IjOZWLzDpZHP";
+  drops = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJ3y03wBlzSp0tooMOILl7hGxIYqxT6IjOZWLzDpZHP rg.grossi@outlook.com";
+  # drops = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJ3y03wBlzSp0tooMOILl7hGxIYqxT6IjOZWLzDpZHP";
   users = [ flakes drops ];
 
   snow = "";
