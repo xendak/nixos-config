013a06ad58b277e1163a76f7bc12e369471f2cfe
Rafael Grossi <rg.grossi@outlook.com>
2024-05-06 12:09:10 -0300

    helix and nvim changes

--- home/common/programs/terminal/nvim/plugins.nix
+++ home/common/programs/terminal/nvim/plugins.nix
@@ -1,4 +1,15 @@
 { pkgs, ... }:
+let
+  nnn = pkgs.vimUtils.buildVimPlugin {
+    name = "nnn";
+    src = pkgs.fetchFromGitHub {
+      owner = "mcchrish";
+      repo = "nnn.vim";
+      rev = "e0104e369508fc12e3651ad4dee20261b5b3e87f";
+      hash = "sha256-ZLokeEA70pdVmKOjK5vB8tRE0zGHicsunIHgW1Px0sw=";
+    };
+  };
+in
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
