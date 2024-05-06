c91d81fd568faa50b9e9496eb8666a586112d29e
Rafael Grossi <rg.grossi@outlook.com>
2024-05-06 10:03:57 -0300

    fix the some refactor

--- home/common/wayland/default.nix
+++ home/common/wayland/default.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     polkit_gnome
     pavucontrol
@@ -15,7 +16,7 @@
   ];
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "application/x-ms-dos-executable" = ["wine.desktop"];
+    "application/x-ms-dos-executable" = [ "wine.desktop" ];
   };
 
   i18n = {
@@ -29,23 +30,22 @@
     };
   };
 
-    sessionVariables = {
-      NIX_AUTO_RUN = "1";
-      MOZ_ENABLE_WAYLAND = 1;
-      ANKI_WAYLAND = "1";
-      NIXOS_OZONE_WL = "1";
-      LIBSEAT_BACKEND = "logind";
-      XCURSOR_SIZE = 36;
-      QT_QPA_PLATFORM = "wayland;xcb";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "0";
-      WLR_NO_HARDWARE_CURSORS = 1;
-      INPUT_METHOD = "fcitx";
-      XIM_SERVERS = "fcitx";
-      XMODIFIERS = "@im=fcitx";
-      XMODIFIER = "@im=fcitx";
-      GTK_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      XDG_CURRENT_DESKTOP = "GNOME"; # fixing QT no icon bullshit
-    };
+  home.sessionVariables = {
+    NIX_AUTO_RUN = "1";
+    MOZ_ENABLE_WAYLAND = 1;
+    ANKI_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
+    LIBSEAT_BACKEND = "logind";
+    XCURSOR_SIZE = 36;
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "0";
+    WLR_NO_HARDWARE_CURSORS = 1;
+    INPUT_METHOD = "fcitx";
+    XIM_SERVERS = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    XMODIFIER = "@im=fcitx";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XDG_CURRENT_DESKTOP = "GNOME"; # fixing QT no icon bullshit
+  };
 }
-
