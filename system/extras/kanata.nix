beabd5f9d89d84629c5f6bd0b62c97641be492cc
Rafael Grossi <rg.grossi@outlook.com>
2024-05-06 11:01:02 -0300

    removed tap-hold on 1, seems awful

--- system/extras/kanata.nix
+++ system/extras/kanata.nix
@@ -8,61 +8,25 @@ let
       lctl lmet lalt           spc           ralt rmet rctl)
     
     (deflayer qwerty
-      grv @1q   2    3    4    5    6    7    8    9    0    -    =    bspc
+    @grvg  1    2    3    4    5    6    7    8    9    0    -    =    bspc
       tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
       @cap a    s    d    f    g    h    j    k    l    ;    '    ret
       lsft z    x    c    v    b    n    m    ,    .    /    rsft
       lctl lmet lalt           spc            ralt rmet rctl)
  
     (deflayer graphite
-      grv @1g   2    3    4    5    6    7    8    9    0    -    =    bspc
+    @grvq  1    2    3    4    5    6    7    8    9    0    -    =    bspc
       tab  /    u    o    f    grv  z    w    d    l    q    [    ]    \
-      @cap i   e    a    h    y    g    s    t    r    n   ,    ret
+      '    i    e    a    h    y    g    s    t    r    n   ,    ret
       lsft x    -    .    p    k    v    c    m    j    b    rsft
       lctl lmet lalt           spc            ralt rmet rctl)
 
-    (deflayer no-mod-graphite
-      grv @1g   2    3    4    5    6    7    8    9    0    -    =    bspc
-      tab  /    u    o    f    grv  z    w    d    l    q    [    ]    \
-      @cap i    e    a    h    y    g    s    t    r    n    ,    ret
-      lsft x    -    .    p    k    v    c    m    j    b    rsft
-      lctl lmet lalt           spc            ralt rmet rctl)
-
-    (defvar
-      ;; Note: consider using different time values for your different fingers.
-      ;; For example, your pinkies might be slower to release keys and index
-      ;; fingers faster.
-      tap-time 200
-      hold-time 150
-
-      left-hand-keys (
-        / u o f grv
-        i e a h y
-        x - . p k
-      )
-      right-hand-keys (
-        z w d l q
-        g s t r n
-        v c m j b
-      )
-    )
-
-    (deffakekeys
-      to-base (layer-switch graphite)
-    )
-    
     (defalias
       ;; tap within 100ms for capslk, hold more than 100ms for lctl
       cap (tap-hold 150 150 bspc lctl)
-      1g (tap-hold-release 150 150 1 (layer-switch qwerty))
-      1q (tap-hold-release 150 150 1 (layer-switch graphite))
+      grvq (tap-hold-release 150 150 grv (layer-switch qwerty))
+      grvg (tap-hold-release 150 150 grv (layer-switch graphite))
 
-      tap (multi
-          (layer-switch no-mod-graphite)
-          (on-idle-fakekey to-base tap 20)
-      )
-      i (tap-hold-release-keys $tap-time $hold-time (multi i @tap) lsft $left-hand-keys)
-      n (tap-hold-release-keys $tap-time $hold-time (multi n @tap) rsft $right-hand-keys)
     )
   '';
 in
