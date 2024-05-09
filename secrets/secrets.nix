6b746d3d68889cd65677e65ae3896af208d69490
Rafael Grossi <rg.grossi@outlook.com>
2024-05-09 12:18:30 -0300

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,8 @@
 let
-  # snow = "";
+  snow = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbcunaQdK1AAI/Yre6HIKOtLQcJRzzuEINSkpH1eCj/ root@Snow";
   dew = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOeSiZ3xn1RH1JURg3zmq2qEOspQOewGIwfQ8QGGETdY root@Dew";
   # recovery = "";
-  systems = [ dew ];
+  systems = [ snow dew ];
 in
 {
   "pw.age".publicKeys = systems;
