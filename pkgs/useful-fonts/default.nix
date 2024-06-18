341cadd83b8971761839b63646e32ea86410269e
Rafael Grossi <rg.grossi@outlook.com>
2024-06-18 15:04:02 -0300

    fix font src/path

--- pkgs/useful-fonts/default.nix
+++ pkgs/useful-fonts/default.nix
@@ -6,20 +6,26 @@ with lib;
   stdenv.mkDerivation {
     name = "useful-fonts";
     version = "1.0";
-    src = ./SofiaPro.ttf;
+    src1 = ./SofiaPro.ttf;
+    src2 = ./FOT-NEWRODINPRO-M.OTF;
+    src3 = ./FOT-RODINHIMAWARIPRO-M.OTF;
+    src4 = ./RODINPRO-B.OTF;
+    src5 = ./PROGGYCLEANNERDFONTMONO-REGULAR.TTF;
+    src6 = ./PROGGYCLEANSZNERDFONTMONO-REGULAR.TTF;
+    src7 = ./PROGGYDOTTED_REGULAR.TTF;
 
     dontUnpack = true;
     dontBuild = true;
     dontConfigure = true;
 
     installPhase = ''
-      install -Dm 644 $src $out/share/fonts/opentype/FOT-NEWRODINPRO-M.OTF
-      install -Dm 644 $src $out/share/fonts/opentype/FOT-RODINHIMAWARIPRO-M.OTF
-      install -Dm 644 $src $out/share/fonts/opentype/RODINPRO-B.OTF
-      install -Dm 644 $src $out/share/fonts/ttf/PROGGYCLEANNERDFONTMONO-REGULAR.TTF
-      install -Dm 644 $src $out/share/fonts/ttf/PROGGYCLEANSZNERDFONTMONO-REGULAR.TTF
-      install -Dm 644 $src $out/share/fonts/ttf/PROGGYDOTTED_REGULAR.TTF
-      install -Dm 644 $src $out/share/fonts/ttf/SofiaPro.ttf
+      install -Dm 644 $src2 $out/share/fonts/opentype/FOT-NEWRODINPRO-M.OTF
+      install -Dm 644 $src3 $out/share/fonts/opentype/FOT-RODINHIMAWARIPRO-M.OTF
+      install -Dm 644 $src4 $out/share/fonts/opentype/RODINPRO-B.OTF
+      install -Dm 644 $src5 $out/share/fonts/ttf/PROGGYCLEANNERDFONTMONO-REGULAR.TTF
+      install -Dm 644 $src6 $out/share/fonts/ttf/PROGGYCLEANSZNERDFONTMONO-REGULAR.TTF
+      install -Dm 644 $src7 $out/share/fonts/ttf/PROGGYDOTTED_REGULAR.TTF
+      install -Dm 644 $src1 $out/share/fonts/ttf/SofiaPro.ttf
     '';
 
     meta = {
