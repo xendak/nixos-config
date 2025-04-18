4365ad6a48a598b64376f49bd1dd8b18cbbf6db4
Rafael Grossi <rg.grossi@outlook.com>
2025-04-18 15:53:26 -0300

    retroarch config update, new way to address cores

--- home/common/games/emulators/retroarch.nix
+++ home/common/games/emulators/retroarch.nix
@@ -3,13 +3,21 @@
   config,
   ...
 }: {
-  home.packages = with pkgs; [
-    (retroarch.override {
-      cores = with libretro; [
+  home.packages = [
+
+    (pkgs.retroarch.withCores (
+      cores: with cores; [
+        ppsspp
         snes9x
+        dolphin
+        mupen64plus
+        desmume
         mgba
-      ];
-    })
+      ] ++ (if pkgs.system == "x86_64-linux" then [
+        # stuff that doesn't happen on ARM
+        pcsx2
+      ] else [])
+    ))
   ];
 
   home.persistence = {
