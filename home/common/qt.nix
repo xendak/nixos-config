f60a643a29676b6b256aff85ecb82c52b5dfb741
Rafael Grossi <rg.grossi@outlook.com>
2025-05-01 00:09:37 -0300

    fixed dolphin no icon

--- home/common/qt.nix
+++ home/common/qt.nix
@@ -1,13 +1,50 @@
-{ pkgs, ...}:
 {
-  # xdg.configFile."Kvantum/kvantum.kvconfig".text = '' '';
+  pkgs,
+  config,
+  ...
+}: {
+  xdg.configFile."qt5ct/qt5ct.conf".text = ''
+    [Appearance]
+    custom_palette=false
+    icon_theme=${config.gtk.iconTheme.name}
+    standard_dialogs=default
+    style=Fusion
+
+    [Fonts]
+    fixed="Sofia Pro,12,-1,5,50,0,0,0,0,0"
+    general="Sofia Pro,12,-1,5,50,0,0,0,0,0"
+
+    [Interface]
+    activate_item_on_single_click=1
+    buttonbox_layout=0
+    cursor_flash_time=1000
+    dialog_buttons_have_icons=1
+    double_click_interval=400
+    gui_effects=@Invalid()
+    keyboard_scheme=2
+    menus_have_icons=true
+    show_shortcuts_in_context_menus=true
+    stylesheets=@Invalid()
+    toolbutton_style=4
+    underline_shortcut=1
+    wheel_scroll_lines=3
+
+    [SettingsWindow]
+    geometry="@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\0\0\0\0\0\0\0\x4\xd3\0\0\x5=\0\0\0\0\0\0\0\0\0\0\x4\xff\0\0\x5w\0\0\0\0\x2\0\0\0\n\0\0\0\0\0\0\0\0\0\0\0\x4\xd3\0\0\x5=)"
+
+    [Troubleshooting]
+    force_raster_widgets=1
+    ignored_applications=@Invalid()
+  '';
+
   home.sessionVariables = {
     QT_STYLE_OVERRIDE = "kvantum";
   };
 
+  # try this to run when icons boom. nv QT_QPA_PLATFORMTHEME=qt5ct QT_STYLE_OVERRIDE=kvantum dolphin
   qt = {
     enable = true;
-    platformTheme.name = "gtk";
+    platformTheme.name = "qtct";
     style.name = "kvantum";
     style.package = with pkgs.stable; [
       libsForQt5.qtstyleplugin-kvantum
