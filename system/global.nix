a945d52d53dab03756368cd48d190ca72ee2d6e5
Rafael Grossi <rg.grossi@outlook.com>
2025-05-02 22:29:16 -0300

    added cachix? lets try

--- system/global.nix
+++ system/global.nix
@@ -79,9 +79,11 @@ in {
       substituters = [
         "https://hyprland.cachix.org"
         "https://ezkea.cachix.org"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
       ];
     };
@@ -117,6 +119,11 @@ in {
 
   # XDG - PORTAL
   environment.systemPackages = with pkgs; [
+    ripgrep
+    fd
+    eza
+    bat
+    
     busybox
     lm_sensors
     agenix
