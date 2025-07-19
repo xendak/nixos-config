a8f858673b877d9596d63dad3eda68153584553b
Rafael Grossi <108767275+xendak@users.noreply.github.com>
2025-07-19 00:33:22 -0300

    feat(emacs/meow): implement comprehensive new keybinding layout Introduces a new, highly customized set of keybindings for Meow, replacing the previous keymap.
    The new layout is more structured, organizing bindings into normal, leader, and motion maps for a more ergonomic modal editing experience.

--- home/common/programs/emacs/binds.el
+++ home/common/programs/emacs/binds.el
@@ -1,8 +1,7 @@
-;; semilin mnx emacs meow
 (use-package meow
   :ensure t
   :init
-  (defconst meow-cheatsheet-layout-rain
+(defconst meow-cheatsheet-layout-rain
     '((<TLDE> "`" "~")
       (<AE01> "1" "!")
       (<AE02> "2" "@")
@@ -17,127 +16,164 @@
       (<AE11> "-" "_")
       (<AE12> "=" "+")
       ;; Top row
-      (<AD01> "," "<")   ; f -> ,
-      (<AD02> "f" "F")   ; l -> f
-      (<AD03> "l" "L")   ; h -> l
-      (<AD04> "k" "K")   ; v -> k
-      (<AD05> "v" "V")   ; z -> v
-      (<AD06> "~" "^")   ; ' -> ~
-      (<AD07> "w" "W")   ; w -> w
-      (<AD08> "o" "O")   ; u -> o
-      (<AD09> "u" "U")   ; o -> u
-      (<AD10> "." ">")   ; y -> .
+      (<AD01> "," "<")
+      (<AD02> "f" "F")
+      (<AD03> "l" "L")
+      (<AD04> "k" "K")
+      (<AD05> "v" "V")
+      (<AD06> "~" "^")
+      (<AD07> "w" "W")
+      (<AD08> "o" "O")
+      (<AD09> "u" "U")
+      (<AD10> "." ">")
       (<AD11> "[" "{")
       (<AD12> "]" "}")
       (<BKSL> "\\" "|")
       ;; Home row
-      (<AC01> "b" "B")   ; s -> b
-      (<AC02> "s" "S")   ; r -> s
-      (<AC03> "h" "H")   ; n -> h
-      (<AC04> "t" "T")   ; t -> t
-      (<AC05> "m" "M")   ; k -> m
-      (<AC06> "c" "C")   ; c -> c
-      (<AC07> "d" "D")   ; d -> d
-      (<AC08> "a" "A")   ; e -> a
-      (<AC09> "e" "E")   ; a -> e
-      (<AC10> "i" "I")   ; i -> i
-      (<AC11> "'" "\"")  ; ; -> '
+      (<AC01> "b" "B")
+      (<AC02> "s" "S")
+      (<AC03> "h" "H")
+      (<AC04> "t" "T")
+      (<AC05> "m" "M")
+      (<AC06> "c" "C")
+      (<AC07> "d" "D")
+      (<AC08> "a" "A")
+      (<AC09> "e" "E")
+      (<AC10> "i" "I")
+      (<AC11> "'" "\"")
       ;; Bottom row
-      (<AB01> "p" "P")   ; x -> p
-      (<AB02> "x" "X")   ; j -> x
-      (<AB03> "j" "J")   ; b -> j
-      (<AB04> "y" "Y")   ; m -> y
-      (<AB05> "q" "Q")   ; q -> q
-      (<AB06> "/" "?")   ; p -> /
-      (<AB07> "g" "G")   ; g -> g
-      (<AB08> ";" ":")   ; , -> ;
-      (<AB09> "_" "_")   ; . -> _
-      (<AB10> "z" "Z")   ; / -> z
-      (<LSGT> "-" "_")))
+      (<AB01> "p" "P")
+      (<AB02> "x" "X")
+      (<AB03> "j" "J")
+      (<AB04> "y" "Y")
+      (<AB05> "q" "Q")
+      (<AB06> "/" "?")
+      (<AB07> "g" "G")
+      (<AB08> ";" ":")
+      (<AB09> "_" "_")
+      (<AB10> "z" "Z")
+      (<LSGT> "_" "-")))
 
-  (defun meow-setup ()
-    (setq meow-cheatsheet-layout meow-cheatsheet-layout-rain)
-    (meow-motion-overwrite-define-key
-     '("<escape>" . ignore))
-    (meow-leader-define-key
-     '("?" . meow-cheatsheet)
-     '("a" . "H-e")
-     '("1" . meow-digit-argument)
-     '("2" . meow-digit-argument)
-     '("3" . meow-digit-argument)
-     '("4" . meow-digit-argument)
-     '("5" . meow-digit-argument)
-     '("6" . meow-digit-argument)
-     '("7" . meow-digit-argument)
-     '("8" . meow-digit-argument)
-     '("9" . meow-digit-argument)
-     '("0" . meow-digit-argument))
-    (meow-normal-define-key
-     '("0" . meow-expand-0)
-     '("1" . meow-expand-1)
-     '("2" . meow-expand-2)
-     '("3" . meow-expand-3)
-     '("4" . meow-expand-4)
-     '("5" . meow-expand-5)
-     '("6" . meow-expand-6)
-     '("7" . meow-expand-7)
-     '("8" . meow-expand-8)
-     '("9" . meow-expand-9)
-     '("-" . negative-argument)
-     '("'" . meow-reverse)                ; ; -> '
-     '(";" . meow-inner-of-thing)         ; , -> ;
-     '("_" . meow-bounds-of-thing)        ; . -> _
-     '("[" . meow-beginning-of-thing)
-     '("]" . meow-end-of-thing)
-     '("z" . meow-visit)                  ; / -> z
-     '("e" . meow-right)                  ; a -> e
-     '("E" . meow-right-expand)           ; A -> E
-     '("j" . meow-back-word)              ; b -> j
-     '("J" . meow-back-symbol)            ; B -> J
-     '("c" . meow-left)                   ; c -> c (unchanged)
-     '("C" . meow-left-expand)            ; C -> C (unchanged)
-     '("d" . meow-next)                   ; d -> d (unchanged)
-     '("D" . meow-next-expand)            ; D -> D (unchanged)
-     '("a" . meow-prev)                   ; e -> a
-     '("A" . meow-prev-expand)            ; E -> A
-     '("f" . meow-find)                   ; l -> f (original find was on l)
-     '("g" . meow-cancel-selection)       ; g -> g (unchanged)
-     '("G" . meow-grab)                   ; G -> G (unchanged)
-     '("i" . meow-append)                 ; i -> i (unchanged)
-     '("I" . meow-open-below)             ; I -> I (unchanged)
-     '("x" . meow-join)                   ; j -> x
-     '("m" . meow-kill)                   ; k -> m
-     '("M" . meow-delete)                 ; K -> M
-     '("l" . meow-line)                   ; h -> l (original line was on h)
-     '("L" . meow-goto-line)              ; H -> L
-     '("y" . meow-mark-word)              ; m -> y
-     '("Y" . meow-mark-symbol)            ; M -> Y
-     '("h" . meow-change)                 ; n -> h
-     '("u" . meow-block)                  ; o -> u
-     '("U" . meow-to-block)               ; O -> U
-     '("/" . meow-yank)                   ; p -> /
-     '("q" . meow-quit)                   ; q -> q (unchanged)
-     '("s" . meow-replace)                ; r -> s
-     '("b" . meow-insert)                 ; s -> b
-     '("B" . meow-open-above)             ; S -> B
-     '("t" . meow-till)                   ; t -> t (unchanged)
-     '("o" . meow-undo)                   ; u -> o
-     '("O" . meow-undo-in-selection)      ; U -> O
-     '("k" . meow-search)                 ; v -> k
-     '("w" . meow-next-word)              ; w -> w (unchanged)
-     '("W" . meow-next-symbol)            ; W -> W (unchanged)
-     '("p" . meow-delete)                 ; x -> p
-     '("P" . meow-backward-delete)        ; X -> P
-     '("." . meow-save)                   ; y -> .
-     '("v" . meow-pop-selection)          ; z -> v
-     '("~" . repeat)                      ; ' -> ~
-     '("(" . puni-backward-sexp)
-     '(")" . puni-forward-sexp)
-     '("^ s" . puni-splice)
-     '("^ r" . puni-raise)
-     '("^ b" . puni-barf-forward)
-     '("^ l" . puni-slurp-forward)
-     '("<escape>" . ignore)))
+(defun meow-setup ()
+(setq meow-cheatsheet-layout meow-cheatsheet-layout-rain)
+(meow-motion-define-key
+   '("l" . meow-prev)      ;; up  
+   '("h" . meow-next)      ;; down
+   '("<escape>" . ignore))
+  
+  ;; Leader key on thumb (r key)
+  ;; This puts frequently used commands on the dominant thumb
+  (meow-leader-define-key
+   ;; Digit arguments
+   '("SPC" . meow-M-x)
+   '("<SPC>" . meow-M-x)
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   ;; Common leader commands on home row
+   '("s" . save-buffer)           ;; save file
+   '("d" . dired)                 ;; file manager
+   '("a" . execute-extended-command) ;; M-x equivalent
+   '("e" . eval-buffer)           ;; evaluate buffer
+   '("i" . imenu)                 ;; navigation
+   '("c" . compile)               ;; compile
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  
+  ;; Normal mode - optimized for your layout
+  (meow-normal-define-key
+   ;; Numbers for expansion (keeping on number row)
+   '("0" . meow-expand-0)
+   '("1" . meow-expand-1)
+   '("2" . meow-expand-2)
+   '("3" . meow-expand-3)
+   '("4" . meow-expand-4)
+   '("5" . meow-expand-5)
+   '("6" . meow-expand-6)
+   '("7" . meow-expand-7)
+   '("8" . meow-expand-8)
+   '("9" . meow-expand-9)
+   '("-" . negative-argument)
+   
+   ;; Movement - using left side home row (l,s,h,t)
+   '("l" . meow-prev)          ;; up (like vim k, but on home row) 
+   '("s" . meow-left)          ;; left (natural position)
+   '("h" . meow-next)          ;; down (like vim j, but on home row)
+   '("t" . meow-right)         ;; right (natural position)
+   
+   ;; Expansion versions (shifted versions)
+   '("L" . meow-prev-expand)
+   '("S" . meow-left-expand)
+   '("H" . meow-next-expand)
+   '("T" . meow-right-expand)
+   
+   ;; Core editing on right home row (c,d,a,e,i)
+   '("c" . meow-change)        ;; change (mnemonic)
+   '("d" . meow-delete)        ;; delete (mnemonic)
+   '("a" . meow-append)        ;; append (mnemonic)
+   '("e" . meow-next-word)     ;; end of word (like vim)
+   '("i" . meow-insert)        ;; insert (mnemonic)
+   
+   ;; Secondary commands on left home row
+   '("z" . meow-search)        ;; next/search (like vim)
+   '("m" . meow-mark-word)     ;; mark (mnemonic)
+   
+   ;; Top row - frequently used commands
+   '("f" . meow-till)          ;; find (mnemonic)
+   '("w" . meow-mark-word)     ;; word (alternative to m)
+   '("o" . meow-block)         ;; block (mnemonic for "open block")
+   '("u" . meow-undo)          ;; undo (mnemonic)
+   
+   ;; Bottom row - less frequent commands  
+   '("p" . meow-yank)          ;; paste (mnemonic)
+   '("x" . meow-line)          ;; line (mnemonic)
+   '("j" . meow-join)          ;; join (mnemonic)
+   '("y" . meow-save)          ;; yank/copy (vim-like)
+   '("q" . meow-quit)          ;; quit (mnemonic)
+   
+   ;; Special positions
+   '("b" . meow-back-word)     ;; back word (caps lock position)
+   '("'" . repeat)             ;; repeat on right pinky
+   '("/" . meow-visit)         ;; visit (right side)
+   '("g" . meow-cancel-selection)  ;; cancel (accessible)
+   '("r" . meow-block)       ;; block select on thumb
+   
+   ;; Upper case versions for related commands
+   '("A" . meow-open-below)
+   '("I" . meow-open-above)  
+   '("D" . meow-backward-delete)
+   '("E" . meow-next-symbol)
+   '("B" . meow-back-symbol)
+   '("F" . meow-find)          ;; till (related to find)
+   '("N" . meow-pop-selection) ;; pop selection
+   '("O" . meow-to-block)
+   '("U" . meow-undo-in-selection)
+   '("W" . meow-mark-symbol)
+   '("X" . meow-goto-line)
+   '("Y" . meow-sync-grab)
+   '("P" . meow-reverse)       ;; reverse (related to yank)
+   '("Q" . meow-goto-line)
+   '("G" . meow-grab)
+   '("J" . meow-bounds-of-thing)
+   
+   ;; Symbol keys
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '(";" . meow-replace)       ;; replace on accessible key
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("_" . meow-swap-grab)
+   '("z" . meow-pop-selection)
+   
+   '("<escape>" . ignore)))
+
   :config
   (meow-setup)
   (meow-global-mode))
--- home/common/programs/emacs/init.nix
+++ home/common/programs/emacs/init.nix
@@ -1,140 +1,151 @@
 { config, ... }:
 {
   home.file = {
-    ".config/emacs/init.el".text = ''
-      (require 'package)
-      (require 'fzf)
+    ".config/emacs/init.el".text = # clisp
+      ''
+        (require 'package)
+        (require 'fzf)
 
-      (add-to-list 'custom-theme-load-path (expand-file-name "themes/" user-emacs-directory))
+        (add-to-list 'custom-theme-load-path (expand-file-name "themes/" user-emacs-directory))
 
-      ;; Load keybinds
-      (let ((bindings-file (expand-file-name "binds.el" user-emacs-directory)))
-        (when (file-exists-p bindings-file)
-          (load-file bindings-file)))
+        ;; Load keybinds
+        (let ((bindings-file (expand-file-name "binds.el" user-emacs-directory)))
+          (when (file-exists-p bindings-file)
+            (load-file bindings-file)))
 
-      (setq default-frame-alist
-            '((menu-bar-lines . 0)
-              (tool-bar-lines . 0)
-              (scroll-bar-width . 0)
-              (vertical-scroll-bars . nil)
-              (horizontal-scroll-bars . nil)))
+        (setq default-frame-alist
+              '((menu-bar-lines . 0)
+                (tool-bar-lines . 0)
+                (scroll-bar-width . 0)
+                (vertical-scroll-bars . nil)
+                (horizontal-scroll-bars . nil)))
 
-      (setq initial-frame-alist default-frame-alist)
+        (setq initial-frame-alist default-frame-alist)
 
-      ;; Function to disable UI elements
-      (defun disable-ui-elements ()
-        "Disable menu bar, tool bar, scroll bar, and tab bar."
-        (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-        (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-        (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-        (when (fboundp 'tab-bar-mode) (tab-bar-mode -1)))
+        ;; Function to disable UI elements
+        (defun disable-ui-elements ()
+          "Disable menu bar, tool bar, scroll bar, and tab bar."
+          (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+          (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+          (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+          (when (fboundp 'tab-bar-mode) (tab-bar-mode -1)))
 
-      ;; Disable immediately
-      (disable-ui-elements)
+        ;; Disable immediately
+        (disable-ui-elements)
 
-      ;; Function to apply mode-line colors from current theme
-      (defun my/apply-theme-modeline-colors ()
-        (when (boundp 'base16-default-theme-colors)
-          (let* ((colors base16-default-theme-colors)
-                 (bg-color (plist-get colors :base02))
-                 (fg-color (plist-get colors :base05))
-                 (inactive-bg (plist-get colors :base01))
-                 (inactive-fg (plist-get colors :base04)))
+        ;; Function to reload keybinds on the fly, while i learn this..
+        (defun my/reload-keybindings ()
+          (interactive)
+          (let ((bindings-file (expand-file-name "binds.el" user-emacs-directory)))
+            (if (file-exists-p bindings-file)
+                (progn
+                  (load-file bindings-file)
+                  (message "Successfully reloaded keybindings from binds.el"))
+              (message "Keybindings file not found: %s" bindings-file))))
+
+        ;; Function to apply mode-line colors from current theme
+        (defun my/apply-theme-modeline-colors ()
+          (when (boundp 'base16-default-theme-colors)
+            (let* ((colors base16-default-theme-colors)
+                   (bg-color (plist-get colors :base02))
+                   (fg-color (plist-get colors :base05))
+                   (inactive-bg (plist-get colors :base01))
+                   (inactive-fg (plist-get colors :base04)))
+              
+              (when (and bg-color fg-color)
+                (set-face-attribute 'mode-line nil
+                                    :background bg-color
+                                    :foreground fg-color))
+              
+              (when (and inactive-bg inactive-fg)
+                (set-face-attribute 'mode-line-inactive nil
+                                    :background inactive-bg
+                                    :foreground inactive-fg)))))
+
+        ;; Function to apply mode-line colors to a specific frame
+        (defun my/apply-theme-modeline-colors-to-frame (frame)
+          "Apply mode-line colors from the current theme to a specific frame."
+          (when (boundp 'base16-default-theme-colors)
+            (let* ((colors base16-default-theme-colors)
+                   (bg-color (plist-get colors :base02))
+                   (fg-color (plist-get colors :base05))
+                   (inactive-bg (plist-get colors :base01))
+                   (inactive-fg (plist-get colors :base04)))
+              
+              (when (and bg-color fg-color)
+                (set-face-attribute 'mode-line frame
+                                    :background bg-color
+                                    :foreground fg-color))
+              
+              (when (and inactive-bg inactive-fg)
+                (set-face-attribute 'mode-line-inactive frame
+                                    :background inactive-bg
+                                    :foreground inactive-fg)))))
+
+        (defun my/setup-frame-visuals (frame)
+          "Apply theme and font settings to a new frame."
+          (with-selected-frame frame
+            (let ((theme-file (expand-file-name "themes/current-theme.el" user-emacs-directory)))
+              (when (file-exists-p theme-file)
+                (load-file theme-file)))
             
-            (when (and bg-color fg-color)
-              (set-face-attribute 'mode-line nil
-                                  :background bg-color
-                                  :foreground fg-color))
+            ;; Set default font for GUI frames only
+            (when (display-graphic-p frame)
+              (add-to-list 'default-frame-alist '(font . "${config.fontProfiles.monospace.family} 14")))
             
-            (when (and inactive-bg inactive-fg)
-              (set-face-attribute 'mode-line-inactive nil
-                                  :background inactive-bg
-                                  :foreground inactive-fg)))))
-
-      ;; Function to apply mode-line colors to a specific frame
-      (defun my/apply-theme-modeline-colors-to-frame (frame)
-        "Apply mode-line colors from the current theme to a specific frame."
-        (when (boundp 'base16-default-theme-colors)
-          (let* ((colors base16-default-theme-colors)
-                 (bg-color (plist-get colors :base02))
-                 (fg-color (plist-get colors :base05))
-                 (inactive-bg (plist-get colors :base01))
-                 (inactive-fg (plist-get colors :base04)))
+            ;; Apply theme-based mode-line colors
+            (my/apply-theme-modeline-colors-to-frame frame)
             
-            (when (and bg-color fg-color)
-              (set-face-attribute 'mode-line frame
-                                  :background bg-color
-                                  :foreground fg-color))
+            ;; Also disable UI elements for new frames
+            (disable-ui-elements)
             
-            (when (and inactive-bg inactive-fg)
-              (set-face-attribute 'mode-line-inactive frame
-                                  :background inactive-bg
-                                  :foreground inactive-fg)))))
+            (message "---> My init.el was loaded successfully!")))
 
-      (defun my/setup-frame-visuals (frame)
-        "Apply theme and font settings to a new frame."
-        (with-selected-frame frame
-          (let ((theme-file (expand-file-name "themes/current-theme.el" user-emacs-directory)))
-            (when (file-exists-p theme-file)
-              (load-file theme-file)))
-          
-          ;; Set default font for GUI frames only
-          (when (display-graphic-p frame)
-            (add-to-list 'default-frame-alist '(font . "${config.fontProfiles.monospace.family} 14")))
-          
-          ;; Apply theme-based mode-line colors
-          (my/apply-theme-modeline-colors-to-frame frame)
-          
-          ;; Also disable UI elements for new frames
-          (disable-ui-elements)
-          
-          (message "---> My init.el was loaded successfully!")))
+        ;; Hook to apply mode-line colors after theme changes
+        (add-hook 'after-load-theme-hook #'my/apply-theme-modeline-colors)
 
-      ;; Hook to apply mode-line colors after theme changes
-      (add-hook 'after-load-theme-hook #'my/apply-theme-modeline-colors)
+        ;; Also apply after theme loading
+        (add-hook 'emacs-startup-hook 
+                  (lambda () 
+                    (run-with-timer 0.1 nil #'my/apply-theme-modeline-colors)))
 
-      ;; Also apply after theme loading
-      (add-hook 'emacs-startup-hook 
-                (lambda () 
-                  (run-with-timer 0.1 nil #'my/apply-theme-modeline-colors)))
+        ;; Hook for new frames created by daemon
+        (add-hook 'after-make-frame-functions #'my/setup-frame-visuals)
 
-      ;; Hook for new frames created by daemon
-      (add-hook 'after-make-frame-functions #'my/setup-frame-visuals)
+        ;; Backup hook
+        (add-hook 'window-setup-hook 'disable-ui-elements)
 
-      ;; Backup hook
-      (add-hook 'window-setup-hook 'disable-ui-elements)
+        (when (and (not (daemonp)) (display-graphic-p))
+          (my/setup-frame-visuals (selected-frame)))
 
-      (when (and (not (daemonp)) (display-graphic-p))
-        (my/setup-frame-visuals (selected-frame)))
+        ;; General Settings
+        (setq initial-scratch-message nil)
+        (setq use-dialog-box nil)
+        (setq inhibit-startup-screen t)
+        (setq package-check-signature nil)
+        (setq read-buffer-completion-ignore-case t
+              read-file-name-completion-ignore-case t
+              completion-ignore-case t)
 
-      ;; General Settings
-      (setq initial-scratch-message nil)
-      (setq use-dialog-box nil)
-      (setq inhibit-startup-screen t)
-      (setq package-check-signature nil)
-      (setq read-buffer-completion-ignore-case t
-            read-file-name-completion-ignore-case t
-            completion-ignore-case t)
+        ;; Backup and Auto-Save Configuration
+        (let ((backup-dir (expand-file-name "backups/" user-emacs-directory))
+              (auto-saves-dir (expand-file-name "auto-saves/" user-emacs-directory)))
+          (make-directory backup-dir t)
+          (make-directory auto-saves-dir t)
+          (setq backup-directory-alist `(("." . ,backup-dir))
+                auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
+                auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
+                tramp-backup-directory-alist `((".*" . ,backup-dir))
+                tramp-auto-save-directory auto-saves-dir))
 
-      ;; Backup and Auto-Save Configuration
-      (let ((backup-dir (expand-file-name "backups/" user-emacs-directory))
-            (auto-saves-dir (expand-file-name "auto-saves/" user-emacs-directory)))
-        (make-directory backup-dir t)
-        (make-directory auto-saves-dir t)
-        (setq backup-directory-alist `(("." . ,backup-dir))
-              auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
-              auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
-              tramp-backup-directory-alist `((".*" . ,backup-dir))
-              tramp-auto-save-directory auto-saves-dir))
+        (setq backup-by-copying t
+              delete-old-versions t
+              version-control t
+              kept-new-versions 2
+              kept-old-versions 1)
 
-      (setq backup-by-copying t
-            delete-old-versions t
-            version-control t
-            kept-new-versions 2
-            kept-old-versions 1)
-
-      (use-package rainbow-delimiters
-        :hook ((lisp-mode emacs-lisp-mode scheme-mode hy-mode) . rainbow-delimiters-mode))
-    '';
+        (use-package rainbow-delimiters
+          :hook ((lisp-mode emacs-lisp-mode scheme-mode hy-mode) . rainbow-delimiters-mode))
+      '';
   };
 }
