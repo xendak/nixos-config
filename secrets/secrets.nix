af920b02597b58dfaeeb04902701ce04111fc0f3
Rafael Grossi <108767275+xendak@users.noreply.github.com>
2025-10-04 10:12:30 -0300

    chore(ssh): fix private/public key & rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,16 +1,22 @@
 let
   snow = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbcunaQdK1AAI/Yre6HIKOtLQcJRzzuEINSkpH1eCj/ root@Snow";
   dew = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOeSiZ3xn1RH1JURg3zmq2qEOspQOewGIwfQ8QGGETdY root@Dew";
+
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWQXNYT4TSaRMLFfc+V99dEC0DeoW6ed1e/cfi5g3Th xendak@nixos";
   # recovery = "";
   systems = [
     snow
     dew
   ];
+  users = [
+    user
+  ];
+  all = users ++ systems;
 in
 {
   "pw.age".publicKeys = systems;
-  "github-token.age".publicKeys = systems;
-  "gemini-api-key.age".publicKeys = systems;
+  "github-token.age".publicKeys = all;
+  "gemini-api-key.age".publicKeys = all;
 }
 
 # ssh-keyscan localhost
@@ -21,3 +27,6 @@ in
 # add a line for the new secret file
 # agenix -e new-secret.age
 # save the file
+
+# edit key with agenix
+# agenix -e file.age
