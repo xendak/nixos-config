46fce495cc5b331c961c5c7581461b2f0d1cc2ff
Rafael Grossi <108767275+xendak@users.noreply.github.com>
2025-10-04 10:53:10 -0300

    chore(fix): oops

--- home/common/programs/terminal/yazi/default.nix
+++ home/common/programs/terminal/yazi/default.nix
@@ -5,26 +5,27 @@
     yazi
   ];
 
-  home.file.".config/yazi/keymap.toml" = pkgs.writeText ''
-    [[mgr.prepend_keymap]]
-    on   = ["f"]
-    run  = "cd --interactive"
-    desc = "Iteractively go to directory"
+  home.file.".config/yazi/keymap.toml".source = pkgs.writeText "keymap.toml" ''
+    #toml
+        [[mgr.prepend_keymap]]
+        on   = ["f"]
+        run  = "cd --interactive"
+        desc = "Iteractively go to directory"
 
-    [[mgr.prepend_keymap]]
-    on   = ["F"]
-    run  = "filter --smart"
-    desc = "Iteractively go to directory"
+        [[mgr.prepend_keymap]]
+        on   = ["F"]
+        run  = "filter --smart"
+        desc = "Iteractively go to directory"
 
-    [[mgr.append_keymap]]
-    on   = ["g", "p"]
-    run = "cd ~/Programming"
-    desc = "Go to Programming directory"
+        [[mgr.append_keymap]]
+        on   = ["g", "p"]
+        run = "cd ~/Programming"
+        desc = "Go to Programming directory"
 
-    [[mgr.append_keymap]]
-    on   = ["g", "F"]
-    run = "cd ~/Flake"
-    desc = "Go to Flake directory"
+        [[mgr.append_keymap]]
+        on   = ["g", "F"]
+        run = "cd ~/Flake"
+        desc = "Go to Flake directory"
   '';
 
   # home.sessionVariables = { };
